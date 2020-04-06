f0275a21b72c7ba7594836e0e703fd4f503621e6
Fredric Lundberg <Fredric.idk@outlook.com>
2020-04-06 18:56:09 +0200

    add addmessage func

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -8,4 +8,16 @@ export const helloWorld = functions.https.onRequest((request: any, response: any
     response.send("Hello from Fredric Lundberg! hihi");
 });
 
+// Take the text parameter passed to this HTTP endpoint and insert it into the
+// Realtime Database under the path /messages/:pushId/original
+exports.addMessage = functions.https.onRequest(async (req: any, res: any) => {
+    // Grab the text parameter.
+    const original = req.query.text;
+    // Push the new message into the Realtime Database using the Firebase Admin SDK.
+    await admin.database().ref('/messages').push({original: original});
+    // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+    //res.redirect(303, snapshot.ref.toString());
+    res.send("Added textinput to database!");
+});
+
 
