0a8dd0ddb016cb240cf64fb7d82dd9fc201c6f86
JoakimBrandt <brjo1621@student.ju.se>
2020-04-17 11:37:36 +0200

    Adds code cleanup

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -10,8 +10,6 @@ const app = express()
 app.use(bodyParser.json())
 admin.initializeApp();
 
-
-
 // GET all positions
 app.get('/positions', async (req: any, res: any) => {
 
@@ -66,35 +64,6 @@ app.post('/positions', async (req: express.Request, res: express.Response) => {
         })
     })
     
-    //await admin.database().ref('/positions').push({position: positionObj})
-
-    
-})
-
-export const helloWorld = functions.https.onRequest((request: any, response: any) => {
-    console.log("Hello everybody!")
-    response.send("Hello from Fredric Lundberg! hihi");
-});
-
-// Take the text parameter passed to this HTTP endpoint and insert it into the
-// Realtime Database under the path /messages/:pushId/original
-exports.addMessage = functions.https.onRequest(async (req: any, res: any) => {
-    // Grab the text parameter.
-    const original = req.query.text;
-    // Push the new message into the Realtime Database using the Firebase Admin SDK.
-    await admin.database().ref('/messages').push({original: original});
-    // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
-    //res.redirect(303, snapshot.ref.toString());
-    res.send("Added textinput to database!");
-});
-
-
-export const storeCoordinates = functions.https.onRequest(async (req: any, res: any) => {
-    //desirilize json object to a custom-made Object POGO
-    //const jsonObj = req.body
-    //validate
-    //place POGO objects coordinates into db
-    //resolve
 })
 
 const api = functions.https.onRequest(app)
