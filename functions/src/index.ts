0cd007abce8b77e3d3aec7bab9ab36e3e9bdf25a
JoakimBrandt <brjo1621@student.ju.se>
2020-04-17 11:12:02 +0200

    Adds fully functional methods
    GET /positions
    POST /positions
    
    Now work as intended and fully functional

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,7 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-//const express = require('express');
 const bodyParser = require('body-parser');
+
 import express = require('express');
 import { IPosition } from './IPosition';
 import { Position } from './Position';
@@ -11,7 +11,32 @@ app.use(bodyParser.json())
 admin.initializeApp();
 
 
-app.post('/position', async (req: express.Request, res: express.Response) => {
+
+// GET all positions
+app.get('/positions', async (req: any, res: any) => {
+
+    if(req.method !== 'GET') {
+        res.status(400).json({
+            message: "Not allowed"
+        })
+        return 
+    };
+
+    var db = admin.database();
+    var ref = db.ref("/positions");
+
+    ref.on("value", function(snapshot: any) {
+        res.status(200).json({
+            positionsArray: snapshot.val()
+        })
+    }, function (errorObject: any) {
+        res.status(500).json({
+            message: "Error: " + errorObject.message()
+        })
+    });
+});
+
+app.post('/positions', async (req: express.Request, res: express.Response) => {
 
     if(req.method !== 'POST') {
         res.status(500).json({
